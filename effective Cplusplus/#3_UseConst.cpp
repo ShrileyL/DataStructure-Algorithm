40c0d502876078ab56194d5b38ff891194a9b8a4
ShirleyL <shulin_liu@foxmail.com>
2016-11-21 17:10:31 +0800

    finish #3 using const member function, modify mistakes

--- effective Cplusplus/#3_UseConst.cpp
+++ effective Cplusplus/#3_UseConst.cpp
@@ -1,3 +1,11 @@
+/**************************************************
+ * Effective C++ 
+ * 
+ * #3 use const member function
+ * 
+ *
+ * 2016/11/21 Shirley test
+ *************************************************/
 
 #include <iostream>
 
@@ -6,48 +14,50 @@ using namespace std;
 class TextBlock
 {
 public:
-	// TextBlock();
-	// ~TextBlock();
-	TextBlock(std::string str){text = str;}
-	const char& operator[](std::size_t position) const
-	{return text[position];};
-	char& operator[](std::size_t position)
-	{return text[position];};
+    // TextBlock();
+    // ~TextBlock();
+    TextBlock(std::string str){text = str;}
+    const char& operator[](std::size_t position) const
+    {return text[position];};
+    char& operator[](std::size_t position)
+    {return text[position];};
 private:
-	std::string text;
+    std::string text;
 };
 
 class CTextBlock
 {
-	CTextBlock(char p){pText = *p;}
-	char& operator[](std::size_t position) const
-	{return pText[position];}
-	void print(std::size_t position){std::cout << pText[position];}
-
-
+public:
+    CTextBlock(char p){pText = &p;}
+    char& operator[](std::size_t position) const
+    {return pText[position];}
+    void print(std::size_t position){std::cout << pText[position];}
+    
+    
 private:
-	char *pText;
+    char *pText;
 };
 
 int main(int argc, char const *argv[])
 {
-	TextBlock tb("Hello");
-
-	std::cout << tb[0];
-
-	const TextBlock ctb("World");
-
-	std::cout << ctb[0];
-
-	tb[0] = 'x';
-
-	//ctb[0] = 'x'; //error! writing a const TextBlock
-
-	CTextBlock cctb('H');
-	char *pc = &cctb[0];
-
-	*pc = 'J';
-	cctb.print();
-
-	return 0;
+    TextBlock tb("Hello");
+    
+    std::cout << tb[0];//output:"H"
+    
+    const TextBlock ctb("World");
+    
+    std::cout << ctb[0];//output:"W"
+    
+    tb[0] = 'x';
+    
+    //ctb[0] = 'x'; //error! writing a const TextBlock
+    
+    CTextBlock cctb('H');
+    char *pc = &cctb[0];
+    //try to modify value in "cctb"
+    *pc = 'J';//make argument to "cctb" through const operator[]!
+    std::cout << *pc << endl;//output:"J".value changed!
+//    cctb.print();
+    
+    return 0;
 }
